debff23b818bd404959a651528001606e0a50f91
Elena Skakovskaya <eoskakovskaya@mail.ru>
2016-11-30 01:02:02 +0300

    ajax query and rendering
    5-9 steps

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
+   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2406398.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      for (var courseNum in response.courses.completed){
+        var course = response.courses.completed[courseNum];
+        $('#badges').append( "<div class='course'>" +
+          "<h3 title='" + course.title + "'>" + course.title + "</h3>" +
+          "<img src='" + course.badge + "'></img>" +
+          "<a href='" + course.url + "' target='_blank' class='btn btn-primary'" +
+          ">See Course</a>"+
+        "</div>" );
+      }
+    }
 
-  // your code will go here
-
+  });
 });
